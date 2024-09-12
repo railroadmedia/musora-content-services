35da168bbfa793958977ad6c0aee738228b0cb7e
peterminhlee <petereminhlee@gmail.com>
2024-09-12 13:48:55 -0700

    added fields to instrucotor

--- src/services/sanity.js
+++ src/services/sanity.js
@@ -822,7 +822,11 @@ export async function fetchLessonContent(railContentId) {
             "coach_profile_image":thumbnail_url.asset->url
           },
           "instructors":instructor[]->name,
-          instructor[]->,
+          "instructor": instructor[]->{
+            name,
+            web_url_path,
+            "coach_card_image": coach_card_image.asset->url,
+          },
           ${assignmentsField}
           video,
           length_in_seconds
