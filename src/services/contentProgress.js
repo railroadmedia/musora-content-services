3eb340aa28b7af3f8b739e10bb8729f30e9f49e1
robwillems <robert@musora.com>
2024-10-17 08:45:55 -0700

    Use constants for keys

--- src/services/contentProgress.js
+++ src/services/contentProgress.js
@@ -6,38 +6,42 @@ import {
     postRecordWatchSession
 } from "./railcontent";
 import {DataContext, ContentProgressVersionKey} from "./dataContext";
+import {fetchParentByRailContentId} from "./sanity";
 
 const STATE_STARTED = 'started';
 const STATE_COMPLETED = 'completed';
+const DATA_KEY_STATUS = 's';
+const DATA_KEY_PROGRESS = 'p';
+const DATA_KEY_RESUME_TIME = 't';
 export let dataContext = new DataContext(ContentProgressVersionKey, fetchContentProgress());
 
 export async function getProgressPercentage(contentId) {
     let data = await dataContext.getData();
-    return data[contentId]?.['p'] ?? 0;
+    return data[contentId]?.[DATA_KEY_PROGRESS] ?? 0;
 }
 
 export async function getProgressState(contentId) {
     let data = await dataContext.getData();
-    return data[contentId]?.['s'] ?? 0;
+    return data[contentId]?.[DATA_KEY_STATUS] ?? 0;
 }
 
 export async function getResumeTimeSeconds(contentId) {
     let data = await dataContext.getData();
-    return data[contentId]?.['t'] ?? 0;
+    return data[contentId]?.[DATA_KEY_RESUME_TIME] ?? 0;
 }
 
 export async function contentStatusStarted(contentId) {
     await dataContext.update(
         function (context) {
             let data = context.data[contentId] ?? [];
-            let progress = data?.['p'] ?? 0;
-            let status = data?.['s'] ?? 0;
+            let progress = data?.[DATA_KEY_PROGRESS] ?? 0;
+            let status = data?.[DATA_KEY_STATUS] ?? 0;
 
             if (status !== STATE_COMPLETED && progress !== 100) {
                 status = STATE_STARTED;
             }
 
-            data['s'] = status;
+            data[DATA_KEY_STATUS] = status;
             context.data[contentId] = data;
         },
         async function () {
@@ -50,11 +54,8 @@ export async function contentStatusCompleted(contentId) {
     await dataContext.update(
         function (context) {
             let data = context.data[contentId] ?? [];
-            let status = data?.['s'] ?? 0;
 
-            status = STATE_COMPLETED;
-
-            data['s'] = status;
+            data[DATA_KEY_STATUS] = STATE_COMPLETED;
             context.data[contentId] = data;
         },
         async function () {
@@ -90,18 +91,22 @@ export async function recordWatchSession({
         function (context) {
             if (contentId) {
                 let data = context.data[contentId] ?? [];
-                let progress = data?.['p'] ?? 0;
-                let status = data?.['s'] ?? 0;
+                let progress = data?.[DATA_KEY_PROGRESS] ?? 0;
+                let status = data?.[DATA_KEY_STATUS] ?? 0;
 
                 if (status !== STATE_COMPLETED && progress !== 100) {
                     status = STATE_STARTED;
                     progress = Math.min(99, Math.round(watchPositionSeconds ?? 0 / Math.max(1, totalDurationSeconds ?? 0) * 100));
                 }
 
-                data['p'] = progress;
-                data['s'] = status;
-                data['t'] = watchPositionSeconds;
+                data[DATA_KEY_PROGRESS] = progress;
+                data[DATA_KEY_STATUS] = status;
+                data[DATA_KEY_RESUME_TIME] = watchPositionSeconds;
                 context.data[contentId] = data;
+
+                let hierarchy = fetchFullContentHeirarchy();
+
+
             }
         },
         async function () {
