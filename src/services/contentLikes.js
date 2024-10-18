2e2bdb0bd0517b20b276f15e401bed9e37dbcf75
robwillems <robert@musora.com>
2024-10-18 14:25:47 -0700

    Progress bubbling and tests

--- src/services/contentLikes.js
+++ src/services/contentLikes.js
@@ -19,12 +19,12 @@ export async function isContentLiked(contentId) {
 export async function likeContent(contentId) {
     contentId = parseInt(contentId);
     await dataContext.update(
-        function (context) {
-            if (!context.data.includes(contentId)) {
-                context.data.push(contentId);
+        function (localContext) {
+            if (!localContext.data.includes(contentId)) {
+                localContext.data.push(contentId);
             }
         },
-        async function(){
+        async function () {
             return postContentLiked(contentId);
         }
     );
@@ -33,15 +33,15 @@ export async function likeContent(contentId) {
 export async function unlikeContent(contentId) {
     contentId = parseInt(contentId);
     await dataContext.update(
-        function (context) {
-            if (context.data.includes(contentId)) {
-                const index = context.data.indexOf(contentId);
+        function (localContext) {
+            if (localContext.data.includes(contentId)) {
+                const index = localContext.data.indexOf(contentId);
                 if (index > -1) { // only splice array when item is found
-                   context.data.splice(index, 1); // 2nd parameter means remove one item only
+                    localContext.data.splice(index, 1); // 2nd parameter means remove one item only
                 }
             }
         },
-        async function(){
+        async function () {
             return postContentUnliked(contentId);
         }
     );
