857c41586d80b6c2671f1ae5804eb2b7a12e903a
robwillems <robert@musora.com>
2024-11-07 09:15:37 -0800

    Fix failing test

--- test/userPermissions.test.js
+++ test/userPermissions.test.js
@@ -1,10 +1,12 @@
 const railContentModule = require('../src/services/railcontent.js')
 const {fetchUserPermissions} = require("../src/services/userPermissions");
+const {initializeTestService} = require("./sanityQueryService.test");
 
 describe('userPermissions', function () {
     let mock = null;
     let testData = [1,52,72,73,77,78,81];
     beforeEach(() => {
+        initializeTestService();
         mock = jest.spyOn(railContentModule, 'fetchUserPermissionsData');
         mock.mockImplementation(() => testData);
     });
