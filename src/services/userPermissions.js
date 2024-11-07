2e74e36c70d7c2694786b35119ee8d51a6407240
robwillems <robert@musora.com>
2024-11-07 09:12:08 -0800

    Add reset option and delay logic

--- src/services/userPermissions.js
+++ src/services/userPermissions.js
@@ -1,12 +1,26 @@
 import {
     fetchUserPermissionsData
 } from "./railcontent";
+import {setLastUpdatedTime, wasLastUpdateOlderThanXSeconds} from "./lastUpdated";
 
+/**
+ * Exported functions that are excluded from index generation.
+ *
+ * @type {string[]}
+ */
+const excludeFromGeneratedIndex = ['wasLastUpdateOlderThanXSeconds'];
 let userPermissionsPromise = null;
+let lastUpdatedKey = `userPermissions_lastUpdated`;
 
 export async function fetchUserPermissions() {
-    if (!userPermissionsPromise) {
+    if (!userPermissionsPromise || wasLastUpdateOlderThanXSeconds(10, lastUpdatedKey)) {
         userPermissionsPromise = fetchUserPermissionsData();
+        setLastUpdatedTime(lastUpdatedKey);
     }
+
     return await userPermissionsPromise;
+}
+
+export async function reset() {
+    userPermissionsPromise = null;
 }
