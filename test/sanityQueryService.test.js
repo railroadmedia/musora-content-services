fe064c2a6d7fe294578962645ff62a3138641ed5
robwillems <robert@musora.com>
2024-11-11 15:22:40 -0800

    Fix missing parent issue and add url

--- test/sanityQueryService.test.js
+++ test/sanityQueryService.test.js
@@ -573,7 +573,7 @@ describe('Sanity Queries', function () {
     });
 
     test('fetchCommentData', async()=>{
-        let data = await fetchCommentModContentData([241251,241252]);
+        let data = await fetchCommentModContentData([241251,241252, 211153]);
         expect(data[241251].title).toBe("Setting Up Your Space");
         expect(data[241251].type).toBe("learning-path-lesson");
         expect(data[241251].parentTitle).toBe("Gear");
