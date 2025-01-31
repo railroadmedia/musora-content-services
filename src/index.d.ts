61d12c2b1bf7d6a421c4d1ec3bcf368d643c2a5d
Robert Willems <robert@musora.com>
2025-01-31 15:44:55 -0800

    Add getLessonsContentRows

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,19 @@
 /*** This file was generated automatically. To recreate, please run `npm run build-index`. ***/
 
-import { globalConfig, initializeService } from './services/config.js'
+import {
+  globalConfig,
+  initializeService
+} from './services/config.js'
 
-import { isContentLiked, likeContent, unlikeContent } from './services/contentLikes.js'
+import {
+  getLessonContentRows
+} from './services/content.js'
+
+import {
+  isContentLiked,
+  likeContent,
+  unlikeContent
+} from './services/contentLikes.js'
 
 import {
   assignmentStatusCompleted,
@@ -17,8 +28,13 @@ import {
   getProgressState,
   getProgressStateByIds,
   getResumeTimeSeconds,
-  recordWatchSession,
+  recordWatchSession
 } from './services/contentProgress.js'
+
+import {
+  setLastUpdatedTime,
+  wasLastUpdateOlderThanXSeconds
+} from './services/lastUpdated.js'
 
 import {
   addItemToPlaylist,
@@ -75,7 +91,7 @@ import {
   setStudentViewForUser,
   unpinPlaylist,
   updatePlaylist,
-  updatePlaylistItem,
+  updatePlaylistItem
 } from './services/railcontent.js'
 
 import {
@@ -116,10 +132,13 @@ import {
   fetchTopLevelParentId,
   fetchUpcomingEvents,
   getSortOrder,
-  jumpToContinueContent,
+  jumpToContinueContent
 } from './services/sanity.js'
 
-import { fetchUserPermissions, reset } from './services/userPermissions.js'
+import {
+  fetchUserPermissions,
+  reset
+} from './services/userPermissions.js'
 
 declare module 'musora-content-services' {
   export {
@@ -200,6 +219,7 @@ declare module 'musora-content-services' {
     getAllCompleted,
     getAllStarted,
     getAllStartedOrCompleted,
+    getLessonContentRows,
     getProgressPercentage,
     getProgressPercentageByIds,
     getProgressState,
@@ -231,10 +251,12 @@ declare module 'musora-content-services' {
     recordWatchSession,
     reportPlaylist,
     reset,
+    setLastUpdatedTime,
     setStudentViewForUser,
     unlikeContent,
     unpinPlaylist,
     updatePlaylist,
     updatePlaylistItem,
+    wasLastUpdateOlderThanXSeconds,
   }
 }
---
+++ src/services/content.js
@@ -0,0 +1,65 @@
+import { fetchByRailContentIds } from './sanity.js'
+
+export async function getLessonContentRows () {
+  //TODO: this should come from backend
+  const rows = [
+    {
+      id: 'recent',
+      title: 'Recent Lessons',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'recommended',
+      title: 'Recommended For You',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'method-progress',
+      title: 'Inspired By Your Method Progress',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'creativity',
+      title: 'Unlock Your Creativity',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'essential-skills',
+      title: 'Essential Skills',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'technique',
+      title: 'Improve Your Technique',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'five-levels',
+      title: '5 Levels of...',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'arpeggios',
+      title: 'Get Started With Arpeggios',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'hears-first-time',
+      title: 'Hears For The First Time',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    },
+    {
+      id: 'ten-minute',
+      title: '10-Minute Workouts',
+      content: [389313, 389314, 389315, 389316, 389317, 389318, 389319, 389320, 389321, 389322]
+    }
+  ]
+
+  const results = await Promise.all(
+    rows.map(async (row) => {
+      const data = await fetchByRailContentIds(row.content)
+      return { id: row.id, title: row.title, items: data }
+    })
+  )
+  return results
+}
