be8e33ee920cee44c4ad688018cae6ece0fbdd96
adrianmcneill <adrian@musora.com>
2025-03-25 16:33:24 -0700

    clean up management

--- src/services/user/management.js
+++ src/services/user/management.js
@@ -14,16 +14,22 @@ const excludeFromGeneratedIndex = []
 
 const baseUrl = `/api/user-management-system/v1`
 
+/**
+ * Block the provided user
+ * @param userId
+ * @returns {Promise<any|string|null>}
+ */
 export async function blockUser(userId) {
   const url = `${baseUrl}/block/${userId}`
-  return postHandler(url)
+  return fetchHandler(url, 'post')
 }
 
+/**
+ * Unblock the provided user. Returns a 422 if the user wasn't blocked
+ * @param userId
+ * @returns {Promise<any|string|null>}
+ */
 export async function unblockUser(userId) {
   const url = `${baseUrl}/unblock/${userId}`
-  return postHandler(url)
-}
-
-async function postHandler(url, body = null) {
-  return fetchHandler(url,'post',null, body)
+  return fetchHandler(url, 'post')
 }
