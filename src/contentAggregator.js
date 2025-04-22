9b229565b89c4f260732fa344df867fbaca1fd54
peterminhlee <60075429+peterminhlee@users.noreply.github.com>
2025-04-22 17:57:17 +0900

    Added like count logic in addContextToContent
    * connected with like count api
    
    * added addLikeCount logic
    
    * fix for mapping
    
    * fix for case where data is in a field instead of direct array
    
    * return fix
    
    * added additional condition for fetchLikeCount
    
    * added condition for resume time data
    
    * Update src/contentAggregator.js
    
    Co-authored-by: Adrian <150092463+adrian-musora@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Adrian <150092463+adrian-musora@users.noreply.github.com>

--- src/contentAggregator.js
+++ src/contentAggregator.js
@@ -1,11 +1,13 @@
 import { getProgressStateByIds, getProgressPercentageByIds, getResumeTimeSecondsByIds } from "./services/contentProgress" 
 import { isContentLikedByIds } from "./services/contentLikes"
+import { fetchLikeCount } from "./services/railcontent"
 
 export const addContextToContent = async (dataPromise, ...dataArgs) => {
   const lastArg = dataArgs[dataArgs.length - 1]
   const options = typeof lastArg === 'object' && !Array.isArray(lastArg) ? lastArg : {}
 
   const {
+    dataField = null,
     addProgressPercentage = false,
     addIsLiked = false,
     addLikeCount = false,
@@ -13,14 +15,20 @@ export const addContextToContent = async (dataPromise, ...dataArgs) => {
     addResumeTimeSeconds = false
   } = options
 
-  const dataParam = lastArg === options ? dataArgs.slice(0, -1) : dataArgs;
+  const dataParam = lastArg === options ? dataArgs.slice(0, -1) : dataArgs
 
   const data = await dataPromise(...dataParam)
   if(!data) return false
 
-  const ids = Array.isArray(data)
-    ? data.map(item => item?.id).filter(Boolean)
-    : [data?.id].filter(Boolean)
+  let ids = []
+
+  if (dataField && data?.[dataField]) {
+    ids = data[dataField].map(item => item?.id).filter(Boolean);
+  } else if (Array.isArray(data)) {
+    ids = data.map(item => item?.id).filter(Boolean);
+  } else if (data?.id) {
+    ids = [data.id]
+  }
 
   if(ids.length === 0) return false
 
@@ -31,17 +39,18 @@ export const addContextToContent = async (dataPromise, ...dataArgs) => {
     addResumeTimeSeconds ? getResumeTimeSecondsByIds(ids) : Promise.resolve(null),
   ])
 
-  const addContext = (item) => ({
+  const addContext = async (item) => ({
     ...item,
     ...(addProgressPercentage ? { progressPercentage: progressPercentageData?.[item.id] } : {}),
     ...(addStatus ? { progressStatus: statusData?.[item.id] } : {}),
     ...(addIsLiked ? { isLiked: isLikedData?.[item.id] } : {}),
+    ...(addLikeCount && ids.length === 1 ? { likeCount: await fetchLikeCount(item.id) } : {}),
     ...(addResumeTimeSeconds ? { resumeTime: resumeTimeData?.[item.id] } : {}),
   })
   
   const newData = Array.isArray(data)
-  ? data.map(addContext)
-  : addContext(data)
+  ? await Promise.all(data.map(addContext))
+  : await addContext(data)
 
   return newData
 }
