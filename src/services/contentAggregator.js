865ef4298122d5858573b30689387cd98222693b
Adrian <150092463+adrian-musora@users.noreply.github.com>
2025-05-08 13:51:43 -0700

    feat (BEH-457) - Add getLastInteractedChild to aggregator (#252)

--- src/services/contentAggregator.js
+++ src/services/contentAggregator.js
@@ -1,6 +1,6 @@
 import { getProgressStateByIds, getProgressPercentageByIds, getResumeTimeSecondsByIds } from "./contentProgress"
 import { isContentLikedByIds } from "./contentLikes"
-import { fetchLikeCount } from "./railcontent"
+import { fetchLikeCount, fetchLastInteractedChild } from "./railcontent"
 
 
 
@@ -15,7 +15,8 @@ export async function addContextToContent(dataPromise, ...dataArgs)
     addIsLiked = false,
     addLikeCount = false,
     addProgressStatus = false,
-    addResumeTimeSeconds = false
+    addResumeTimeSeconds = false,
+    addLastInteractedChild = false,
   } = options
 
   const dataParam = lastArg === options ? dataArgs.slice(0, -1) : dataArgs
@@ -35,13 +36,15 @@ export async function addContextToContent(dataPromise, ...dataArgs)
 
   if(ids.length === 0) return false
 
-  const [progressPercentageData, progressStatusData, isLikedData, resumeTimeData] = await Promise.all([
+  const [progressPercentageData, progressStatusData, isLikedData, resumeTimeData, lastInteractedChildData] = await Promise.all([
     addProgressPercentage ? getProgressPercentageByIds(ids) : Promise.resolve(null),
     addProgressStatus ? getProgressStateByIds(ids) : Promise.resolve(null),
     addIsLiked ? isContentLikedByIds(ids) : Promise.resolve(null),
     addResumeTimeSeconds ? getResumeTimeSecondsByIds(ids) : Promise.resolve(null),
+    addLastInteractedChild ? fetchLastInteractedChild(ids)  : Promise.resolve(null),
   ])
-
+  console.log('ids', ids)
+  console.log('lastInteractedChildData', lastInteractedChildData)
   const addContext = async (item) => ({
     ...item,
     ...(addProgressPercentage ? { progressPercentage: progressPercentageData?.[item.id] } : {}),
@@ -49,12 +52,17 @@ export async function addContextToContent(dataPromise, ...dataArgs)
     ...(addIsLiked ? { isLiked: isLikedData?.[item.id] } : {}),
     ...(addLikeCount && ids.length === 1 ? { likeCount: await fetchLikeCount(item.id) } : {}),
     ...(addResumeTimeSeconds ? { resumeTime: resumeTimeData?.[item.id] } : {}),
+    ...(addLastInteractedChild ? { lastInteractedChild: lastInteractedChildData?.[item.id] } : {}),
   })
-
-  const newData = Array.isArray(data)
-  ? await Promise.all(data.map(addContext))
-  : await addContext(data)
-
-  return newData
+  if (dataField) {
+    data[dataField] = Array.isArray(data[dataField])
+        ? await Promise.all(data[dataField].map(addContext))
+        : await addContext(data[dataField])
+    return data
+  } else {
+    return Array.isArray(data)
+        ? await Promise.all(data.map(addContext))
+        : await addContext(data)
+  }
 }
 
