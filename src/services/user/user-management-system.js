f76898b6791496732fb73ec72502f1cf0c651e25
Alexandre Severo <alexandre.severo@me.com>
2025-05-13 12:43:22 -0700

    add docs

--- src/services/user/user-management-system.js
+++ src/services/user/user-management-system.js
@@ -3,4 +3,5 @@
  * @property {module:Sessions} Sessions
  * @property {module:Permissions} Permissions
  * @property {module:UserManagement} UserManagement
+ * @property {module:Interests} Interests
  */
