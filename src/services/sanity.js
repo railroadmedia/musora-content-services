80b8e8f0983f2e811e7e5bdfe18da8eeb0908375
Daniel Austin <daniel@abbytreasures.com>
2025-05-15 10:14:45 -0700

    add field

--- src/services/sanity.js
+++ src/services/sanity.js
@@ -383,7 +383,8 @@ export async function fetchUpcomingEvents(brand, { page = 1, limit = 10 } = {}) 
         published_on,
         "type": _type,
         web_url_path,
-        "permission_id": permission[]->railcontent_id,`
+        "permission_id": permission[]->railcontent_id,
+        event_calendar_unique_key`
   const query = buildRawQuery(
     `_type in ${typesString} && brand == '${brand}' && published_on > '${now}' && status == 'scheduled'`,
     fields,
