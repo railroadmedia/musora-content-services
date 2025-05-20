d370fd0669ee12578210baeb2062c807c48b4565
Alexandre Severo <alexandre.severo@me.com>
2025-05-20 12:51:41 -0700

    feat(MU2-390):  other stats (#246)
    * fetch longest streaks by userid
    
    * other stats
    
    * indexes
    
    * docs
    
    * refactor

---
+++ src/services/user/profile.js
@@ -0,0 +1,33 @@
+/**
+ * @module UserProfile
+ */
+import { fetchJSONHandler } from '../../lib/httpHelper.js'
+import { globalConfig } from '../config.js'
+import { calculateLongestStreaks } from '../userActivity.js'
+import './types.js'
+
+const baseUrl = `/api/user-management-system`
+
+/**
+ * @param {number|null} userId - The user ID to reset permissions for.
+ * @returns {Promise<OtherStatsDTO>}
+ */
+export async function otherStats(userId = globalConfig.sessionConfig.userId) {
+  const [otherStats, longestStreaks] = await Promise.all([
+    fetchJSONHandler(`${baseUrl}/v1/users/${userId}/permissions`, 'delete'),
+    calculateLongestStreaks(userId),
+  ])
+
+  return {
+    ...otherStats,
+    longest_day_streak: {
+      type: 'day',
+      length: longestStreaks.longestDailyStreak,
+    },
+    longest_week_streak: {
+      type: 'week',
+      length: longestStreaks.longestWeeklyStreak,
+    },
+    total_practice_time: longestStreaks.totalPracticeSeconds,
+  }
+}
