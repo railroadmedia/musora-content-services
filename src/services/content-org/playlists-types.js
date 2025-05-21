114a4eaec11ea09ad81f06b5ca9d0d6294785348
Adrian <adrian@musora.com>
2025-05-21 10:21:35 -0700

    Update additem playlist

--- src/services/content-org/playlists-types.js
+++ src/services/content-org/playlists-types.js
@@ -27,9 +27,7 @@
  * @typedef AddItemToPlaylistDTO
  * @property {number} content_id - The ID of the content to add to the playlist(s).
  * @property {Array<number>} playlist_id - An array of playlist IDs where the content should be added.
- * @property {boolean} import_full_soundslice_assignment - Flag to include full Soundslice assignments.
- * @property {boolean} import_instrumentless_soundslice_assignment - Flag to include instrumentless Soundslice assignments.
+ * @property {number} position - Optional playlist position requirement
  * @property {boolean} import_high_routine - Flag to include high routine content.
  * @property {boolean} import_low_routine - Flag to include low routine content.
- * @property {boolean} import_all_assignments - Flag to include all Soundslice assignments if true.
  */
