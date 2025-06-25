309a9e7fb5fd6b6280ae1fae285ca216a1ddf6c8
Daniel Austin <daniel@abbytreasures.com>
2025-06-25 09:58:42 -0700

    change guided-courses.js to typescript

--- src/services/content-org/guided-courses.ts
+++ src/services/content-org/guided-courses.ts
@@ -5,26 +5,22 @@ import { globalConfig } from '../config.js'
 import { fetchHandler } from '../railcontent.js'
 import './playlists-types.js'
 
-/**
- * Exported functions that are excluded from index generation.
- *
- * @type {string[]}
- */
-const excludeFromGeneratedIndex = []
 
-const BASE_PATH = `/api/content-org`
+const excludeFromGeneratedIndex: string[] = []
+
+const BASE_PATH: string = `/api/content-org`
 
 export async function enrollUserInGuidedCourse(guidedCourse) {
-  const url = `${BASE_PATH}/v1/user/guided-courses/enroll-user/${guidedCourse}`
+  const url: string = `${BASE_PATH}/v1/user/guided-courses/enroll-user/${guidedCourse}`
   return await fetchHandler(url, 'POST')
 }
 
 export async function unEnrollUserInGuidedCourse(guidedCourse) {
-  const url = `${BASE_PATH}/v1/user/guided-courses/un-enroll-user/${guidedCourse}`
+  const url: string = `${BASE_PATH}/v1/user/guided-courses/un-enroll-user/${guidedCourse}`
   return await fetchHandler(url, 'POST')
 }
 
 export async function fetchEnrollmentPageMetadata(guidedCourse) {
-  const url = `${BASE_PATH}/v1/user/guided-courses/enrollment/${guidedCourse}`
+  const url: string = `${BASE_PATH}/v1/user/guided-courses/enrollment/${guidedCourse}`
   return await fetchHandler(url, 'GET')
 }
