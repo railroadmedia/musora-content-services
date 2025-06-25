bd12468929cbf013e70453fcaaa2b86a484186ac
Sheldon Baker <sbaker@musora.com>
2025-06-25 14:14:22 -0700

    resolve inconsistent async usage

--- src/infrastructure/local-cache/adapters/index.ts
+++ src/infrastructure/local-cache/adapters/index.ts
@@ -5,7 +5,7 @@ export interface ILocalCache {
   getItem(key: string): Promise<string | null>;
   setItem(key: string, value: string): Promise<void>;
   removeItem(key: string): Promise<void>;
-  getKeys(startsWith: string): Promise<string[]>;
+  getKeys(startsWith?: string): Promise<string[]>;
 }
 
 export { MobileAppCache, WebCache };
