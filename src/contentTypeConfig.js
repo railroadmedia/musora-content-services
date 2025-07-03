9f742b4fdb073e75b1798171242938abb24ed745
Erez <etall@musora.com>
2025-07-03 13:20:41 -0700

    added GC type to contentTypeConfig.js

--- src/contentTypeConfig.js
+++ src/contentTypeConfig.js
@@ -154,7 +154,7 @@ export const individualLessonsTypes = [
   ...studentArchivesLessonTypes
 ];
 
-export const coursesLessonTypes = ['course', 'pack','spotlight'];
+export const coursesLessonTypes = ['course', 'pack','spotlight', 'guided-course'];
 export const showsLessonTypes = ['diy-drum-experiment','exploring-beats','in-rhythm',  'rhythmic-adventures-of-captain-carson','rhythms-from-another-planet','study-the-greats'];
 export const collectionLessonTypes = [
     ...coursesLessonTypes,
