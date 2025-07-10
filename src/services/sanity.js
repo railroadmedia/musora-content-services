667ed0ab8a022d9f0157bb84c41742217e888274
Adrian <adrian@musora.com>
2025-07-10 12:52:25 -0700

    formatting?

--- src/services/sanity.js
+++ src/services/sanity.js
@@ -2356,17 +2356,17 @@ export async function fetchTabData(
 
   filter = `brand == "${brand}" ${includedFieldsFilter} ${progressFilter}`
   const childrenFilter = await new FilterBuilder(``, { isChildrenFilter: true }).buildFilter()
-  entityFieldsString = ` ${fieldsString},
-                                    'children': child[${childrenFilter}]->{'id': railcontent_id},
-                                    'lesson_count': coalesce(count(child[${childrenFilter}]->), 0) ,
-                                    'length_in_seconds': coalesce(
+  entityFieldsString =
+    ` ${fieldsString},
+    'children': child[${childrenFilter}]->{'id': railcontent_id},
+    'lesson_count': coalesce(count(child[${childrenFilter}]->), 0),
+    'length_in_seconds': coalesce(
       math::sum(
         select(
           child[${childrenFilter}]->length_in_seconds
         )
       ),
       length_in_seconds
-      ),
     ),`
 
   const filterWithRestrictions = await new FilterBuilder(filter, {}).buildFilter()
