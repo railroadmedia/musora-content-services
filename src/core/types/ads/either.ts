ae10cf6906f37593ef511a32b2cb83f67b38d7a6
Alexandre Severo <alexandre.severo@me.com>
2025-07-17 11:48:56 -0700

    update comment

--- src/core/types/ads/either.ts
+++ src/core/types/ads/either.ts
@@ -2,6 +2,6 @@ import { Coproduct } from './coproduct'
 
 /**
  * @extends Coproduct
- * It is often used to represent computations that can fail or succeed, where left is error and right is success.
+ * Coproduct often used to represent computations that can fail or succeed, where left is error and right is success.
  */
 export abstract class Either<L, R> extends Coproduct<L, R> {}
