83c9ada12b297a6dc8b236f8fc338ce65997b21e
Roxana Riza <roxana_r2000@yahoo.com>
2025-07-21 10:23:00 +0300

    Update MCS to use new endpoint to get recent activities tabs with data

--- src/services/userActivity.js
+++ src/services/userActivity.js
@@ -1474,4 +1474,21 @@ function getFirstLeafLessonId(data) {
   return data.lessons ? findFirstLeaf(data.lessons) : null
 }
 
+export async function fetchRecentActivitiesActiveTabs() {
+  const url = `/api/user-management-system/v1/activities/tabs`
+  try {
+    const tabs = await fetchHandler(url, 'GET');
+    const activitiesTabs = [];
+
+    tabs.forEach(tab => {
+      activitiesTabs.push({ name: tab.label });
+    });
+
+    return activitiesTabs;
+  } catch (error) {
+    console.error('Error fetching activity tabs:', error);
+    return [];
+  }
+}
+
 
