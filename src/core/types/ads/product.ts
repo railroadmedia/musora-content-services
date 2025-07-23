6603b3dc4d2436bebffd23fcfb26b26276687b57
Alexandre Severo <alexandre.severo@me.com>
2025-07-23 09:57:59 -0700

    clean up product

--- src/core/types/ads/product.ts
+++ src/core/types/ads/product.ts
@@ -2,13 +2,23 @@ import { FoldableProduct } from '../interfaces/foldable'
 import { Tappable } from '../interfaces/tappable'
 
 /** A product type that combines two values of potentially different types. */
-export abstract class Product<F, S> implements Tappable<F | S>, FoldableProduct<F, S> {
+export class Product<F, S> implements Tappable<F | S>, FoldableProduct<F, S> {
+  constructor(
+    private readonly firstValue: F,
+    private readonly secondValue: S
+  ) {}
+
   static of<F, S>(first: F, second: S): Product<F, S> {
-    return new Tuple(first, second)
+    return new Product(first, second)
   }
 
-  abstract first(): F
-  abstract second(): S
+  first(): F {
+    return this.firstValue
+  }
+
+  second(): S {
+    return this.secondValue
+  }
 
   tap(fn: (value: F | S) => void): this {
     fn(this.first())
@@ -25,22 +35,5 @@ export abstract class Product<F, S> implements Tappable<F | S>, FoldableProduct<
     acc = fn(acc, this.first())
     acc = fn(acc, this.second())
     return acc
-  }
-}
-
-export class Tuple<F, S> extends Product<F, S> {
-  constructor(
-    private readonly firstValue: F,
-    private readonly secondValue: S
-  ) {
-    super()
-  }
-
-  first(): F {
-    return this.firstValue
-  }
-
-  second(): S {
-    return this.secondValue
   }
 }
