7b47ade1149d32f92f3137d177b2c761c2fb78de
Sheldon Baker <sbaker@musora.com>
2025-08-26 12:31:56 -0700

    setup flow

--- src/services/sync/setup.ts
+++ src/services/sync/setup.ts
@@ -1,16 +1,19 @@
-import { DatabaseAdapter } from "@nozbe/watermelondb";
-import SyncStoreOrchestrator from "./orchestrator";
+import { type SyncContext } from "./index";
 
 // teardown (ideally occurring on logout) should:
 // 1. stop all sync strategies
-// 2. cancel any scheduled fetch requests that came from those sync strategies
+// 2. cancel any scheduled fetch requests that came from those sync strategies (handled by orchestrator abort controller)
 // 3. reset local databases
-// 4. purge any databases that are no longer referenced in schema (possible if user logs out after a schema change)
+// 4. purge any databases that are no longer referenced in schema (possible if user logs out after a schema change)?
 
-export default function syncSetup({ adapter, orchestrator }: { adapter: DatabaseAdapter, orchestrator: SyncStoreOrchestrator }) {
+export default function syncSetup({ orchestrator, databases }: SyncContext) {
   orchestrator.start()
 
-  return () => {
+  return async () => {
     orchestrator.stop()
+    await Promise.all(databases.map(db => db.write(() => db.unsafeResetDatabase())))
+
+    // TODO - purge entire adapter?
+    // no functionality built-in - would have to extend adapter
   }
 }
