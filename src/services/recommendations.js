db8a16ee1244b9092542edb9566088d8aa02cd89
Alexandre Severo <alexandre.severo@me.com>
2025-08-28 11:53:30 -0700

    update recommendations

--- src/services/recommendations.js
+++ src/services/recommendations.js
@@ -4,11 +4,21 @@
 
 import { globalConfig } from './config.js'
 import { HttpClient } from '../infrastructure/http/HttpClient'
+import { Either } from '../core/types/ads/either.js'
+
+/**
+ * Exported functions that are excluded from index generation.
+ *
+ * @type {string[]}
+ */
+const excludeFromGeneratedIndex = []
 
 const handleError = (message, error) => {
   console.error(message, error)
   return error
 }
+
+const baseURL = 'https://recommender.musora.com'
 
 /**
  * Fetches similar content to the provided content id
@@ -16,7 +26,7 @@ const handleError = (message, error) => {
  * @param {integer} content_id - The ID of the content to find similar items for
  * @param {brand} brand - brand of the content to filter
  * @param {integer} count - number of items to return
- * @returns {Promise<Object|null>} - Returns the content_ids sorted by rank (most significant first)
+ * @returns {Promise<Either<HttpError, Array<number>>} - Returns the content_ids sorted by rank (most significant first)
  * @example
  * rankItems('drumeo', 1113)
  *   .then(status => console.log(status))
@@ -32,33 +42,16 @@ export async function fetchSimilarItems(content_id, brand, count = 10) {
     content_ids: content_id,
     num_similar: count + 1, // because the content itself is sometimes returned
   }
-  const url = `/similar_items/`
-  const httpClient = new HttpClient(
-    globalConfig.recommendationsConfig.baseUrl,
-    globalConfig.recommendationsConfig.token
-  )
-  return httpClient.post(url, data).then((r) =>
-    r.fold(
-      (error) => handleError('Fetch similar items error:', error),
-      // we requested count + 1 then filtered out the extra potential value, so we need slice to the correct size if necessary
-      (data) => data['similar_items'].filter((item) => item !== content_id).slice(0, count)
+  return new HttpClient(baseURL)
+    .post('/similar_items/', data)
+    .then((res) =>
+      res
+        .ltap((err) => handleError('Fetch error:', err))
+        .map((data) => data['similar_items'].filter((item) => item !== content_id).slice(0, count))
     )
-  )
 }
 
-const parseRankedCategories = (data) => {
-  let rankedCategories = []
-
-  for (const rankedPlaylist of data) {
-    rankedCategories.push({
-      slug: rankedPlaylist.playlist_id,
-      items: rankedPlaylist.ranked_items,
-    })
-  }
-  return rankedCategories
-}
-
-const defaultSorting = (categories) => {
+const getDefaultSorting = (categories) => {
   const defaultSorting = []
   for (const slug in categories) {
     defaultSorting.push({
@@ -69,12 +62,24 @@ const defaultSorting = (categories) => {
   return defaultSorting
 }
 
+const parseCategories = (data) => {
+  let rankedCategories = []
+
+  for (const rankedPlaylist of data['ranked_playlists']) {
+    rankedCategories.push({
+      slug: rankedPlaylist.playlist_id,
+      items: rankedPlaylist.ranked_items,
+    })
+  }
+  return rankedCategories
+}
+
 /**
  * Sorts the provided categories based on the user's match
  *
  * @param {brand} brand - brand of the content to filter
  * @param {Object} categories - Keyed arrays of content ids
- * @returns {Promise<Object|null>} - Returns the content_ids sorted by rank (most significant first), elements in each category are also ranked
+ * @returns {Promise<Array<number>>} - Returns the content_ids sorted by rank (most significant first), elements in each category are also ranked
  * @example
  * rankCategories('drumeo', {
  *                            1: [111222, 23120, 402199],
@@ -93,15 +98,13 @@ export async function rankCategories(brand, categories) {
     user_id: globalConfig.sessionConfig.userId,
     playlists: categories,
   }
-  const url = `/rank_each_list/`
-  const httpClient = new HttpClient(
-    globalConfig.recommendationsConfig.baseUrl,
-    globalConfig.recommendationsConfig.token
-  )
-  return httpClient.post(url, data).then((r) =>
-    r.fold(
-      (_error) => defaultSorting(categories),
-      (data) => parseRankedCategories(data['ranked_playlists'])
+  return new HttpClient(baseURL).post('/rank_each_list/', data).then((res) =>
+    res.fold(
+      (err) => {
+        console.error('RankCategories fetch error:', err)
+        return getDefaultSorting(categories)
+      },
+      (data) => parseCategories(data)
     )
   )
 }
@@ -111,7 +114,7 @@ export async function rankCategories(brand, categories) {
  *
  * @param {brand} brand - brand of the content to filter
  * @param {Array<string>} content_ids - The IDs of the content to rank
- * @returns {Promise<Object|null>} - Returns the content_ids sorted by rank (most significant first)
+ * @returns {Promise<Array<number>>} - Returns the content_ids sorted by rank (most significant first)
  * @example
  * rankItems('drumeo', ([111222, 23120, 402199])
  *   .then(status => console.log(status))
@@ -126,30 +129,37 @@ export async function rankItems(brand, content_ids) {
     user_id: globalConfig.sessionConfig.userId,
     content_ids: content_ids,
   }
-  const url = `/rank_items/`
-  const httpClient = new HttpClient(
-    globalConfig.recommendationsConfig.baseUrl,
-    globalConfig.recommendationsConfig.token
-  )
-  return httpClient.post(url, data).then((r) =>
-    r
-      .ltap((_e) => handleError('Rank items fetch error:', error))
-      .fold(
-        (_error) => content_ids,
-        (data) => data['ranked_content_ids']
-      )
+  return new HttpClient(baseURL).post('/rank_items/', data).then((res) =>
+    res.fold(
+      (err) => {
+        console.error('rankItems fetch error:', err)
+        return content_ids
+      },
+      (data) => data['ranked_items']
+    )
   )
 }
 
+/**
+ * Fetches recommended content for a specific brand and section.
+ * If no section is provided, it fetches general recommendations for the brand.
+ * The section parameter is optional and can be used to filter recommendations by a specific section.
+ * The section value is converted to uppercase and any hyphens are replaced with underscores before being sent to the API.
+ * @param {string} brand - The brand for which to fetch recommendations.
+ * @param {Object} options - Optional parameters.
+ * @param {string} options.section - The section to filter recommendations by (optional).
+ * @returns {Promise<Array<Object>>} - Returns the recommended content or null in case of an error.
+ */
 export async function recommendations(brand, { section = '' } = {}) {
   section = section.toUpperCase().replace('-', '_')
   const sectionString = section ? `&section=${section}` : ''
   const url = `/api/content/v1/recommendations?brand=${brand}${sectionString}`
-  const httpClient = new HttpClient(globalConfig.baseUrl, globalConfig.sessionConfig.token)
-  return httpClient.get(url).then((r) =>
-    r.fold(
-      (error) => handleError('Recommendations fetch error:', error),
-      (data) => data
-    )
+  const data = await new HttpClient(globalConfig.baseUrl, globalConfig.sessionConfig.token).get(url)
+  return data.fold(
+    (err) => {
+      console.error('Recommendations fetch error:', err)
+      return []
+    },
+    (data) => data
   )
 }
