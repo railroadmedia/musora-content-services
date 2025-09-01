2e753b758934a1d2d202af7dd3acf0fc5c62671f
Sheldon Baker <sbaker@musora.com>
2025-09-01 14:25:36 -0700

    refactor setup

--- src/services/sync/database/factory.ts
+++ src/services/sync/database/factory.ts
@@ -1,14 +1,7 @@
 import type { DatabaseAdapter } from '../adapters/factory'
-import { Database } from '@nozbe/watermelondb'
+import { Database, Model } from '@nozbe/watermelondb'
 
-// TODO - fix this once we don't have mcs build-index constraint (i.e., a models/index.ts file)
-import ContentLike from '../models/ContentLike'
-import ContentProgress from '../models/ContentProgress'
-import ContentPractice from '../models/ContentPractice'
-
-type ModelClasses = typeof ContentLike | typeof ContentProgress | typeof ContentPractice
-
-export default function syncDatabaseFactory(adapter: DatabaseAdapter, modelClasses: ModelClasses[]) {
+export default function syncDatabaseFactory(adapter: DatabaseAdapter, modelClasses: (typeof Model)[]) {
   return new Database({
     adapter,
     modelClasses
