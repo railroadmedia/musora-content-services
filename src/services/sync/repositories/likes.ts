a9b12f7232aed9f2a7cad8d0d3182ed049890ff2
Sheldon Baker <sbaker@musora.com>
2025-09-02 16:05:48 -0700

    move store logic to repo

--- src/services/sync/repositories/likes.ts
+++ src/services/sync/repositories/likes.ts
@@ -3,9 +3,11 @@ import ContentLike from "../models/ContentLike";
 
 export default class LikesRepository extends SyncRepository<ContentLike> {
   async create(contentId: number) {
-    return await this.upsert(LikesRepository.generateId(contentId), record => {
-      record.content_id = contentId;
+    const record = await this.upsert(LikesRepository.generateId(contentId), r => {
+      r.content_id = contentId;
     })
+
+    return await this.pushOneEagerly(record)
   }
 
   private static generateId(contentId: number) {
