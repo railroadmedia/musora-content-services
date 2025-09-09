e37c5fb4c5861745973352500258101e2c280585
David Douglas <ddouglas@musora.com>
2025-09-09 09:46:54 -0700

    feat(988): Return json of the response

--- src/services/payments.js
+++ src/services/payments.js
@@ -5,5 +5,6 @@ import { globalConfig } from './config.js'
 import { fetchHandler } from '../lib/httpHelper.js'
 
 export async function fetchCustomerPayments() {
-    return await fetchHandler(globalConfig.baseUrl + '/api/customer/orders/v1', globalConfig.sessionConfig.authToken, 'get')
+    const response = await fetchHandler(globalConfig.baseUrl + '/api/customer/orders/v1', globalConfig.sessionConfig.authToken, 'get')
+    return await response.json()
 }
