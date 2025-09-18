fdb184acd2cb01c628c80654060c544f50fa6792
Roxana Riza <roxana_r2000@yahoo.com>
2025-09-18 15:45:22 +0300

    New method for follow/unfollow threads

--- src/services/forums/threads.ts
+++ src/services/forums/threads.ts
@@ -28,3 +28,27 @@ export async function createThread(
   const httpClient = new HttpClient(globalConfig.baseUrl)
   return httpClient.post<ForumThread>(`${baseUrl}/v1/categories/${categoryId}/threads`, params)
 }
+
+/**
+ * Follow a thread.
+ *
+ * @param {number} threadId - The ID of the thread to lock.
+ * @return {Promise<void>} - A promise that resolves when the thread is locked.
+ * @throws {HttpError} - If the request fails.
+ */
+export async function followThread(threadId: number): Promise<void> {
+  const httpClient = new HttpClient(globalConfig.baseUrl)
+  return httpClient.post<void>(`${baseUrl}/v1/threads/${threadId}/follow`, {})
+}
+
+/**
+ * Unlock a thread to allow further posts.
+ *
+ * @param {number} threadId - The ID of the thread to unlock.
+ * @return {Promise<void>} - A promise that resolves when the thread is unlocked.
+ * @throws {HttpError} - If the request fails.
+ */
+export async function unfollowThread(threadId: number): Promise<void> {
+  const httpClient = new HttpClient(globalConfig.baseUrl)
+  return httpClient.delete<void>(`${baseUrl}/v1/threads/${threadId}/follow`)
+}
