d41f8e6a85091705affe9d263f9dc9c14860ab30
Daniel Austin <51715647+Meowbox64@users.noreply.github.com>
2025-10-06 14:48:59 -0700

    feat: set up table schema, model, and dummy repository

--- src/services/sync/models/ContentProgress.ts
+++ src/services/sync/models/ContentProgress.ts
@@ -3,4 +3,39 @@ import { SYNC_TABLES } from '../schema'
 
 export default class ContentProgress extends BaseModel {
   static table = SYNC_TABLES.CONTENT_PROGRESS
+
+
+  get content_id() {
+    return this._getRaw('content_id') as number
+  }
+  get state() {
+    return this._getRaw('state') as string
+  }
+  get progress_percent() {
+    return this._getRaw('progress_percent') as number
+  }
+  get parent_type() {
+    return this._getRaw('parent_type') as number
+  }
+  get parent_id() {
+    return this._getRaw('parent_id') as number
+  }
+
+  set content_id(value: number) {
+    this._setRaw('content_id', value)
+  }
+  set state(value: string) {
+    this._setRaw('state', value)
+  }
+  set progress_percent(value: number) {
+    this._setRaw('progress_percent', value)
+  }
+  set parent_type(value: number) {
+    this._setRaw('parent_type', value)
+  }
+  set parent_id(value: number) {
+    this._setRaw('parent_id', value)
+  }
+
+
 }
