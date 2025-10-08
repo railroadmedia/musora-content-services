c7bf52ef0633fda20ff5e78898e5ffc2e2a1c899
Daniel Austin <51715647+Meowbox64@users.noreply.github.com>
2025-10-08 10:18:17 -0700

    fix: generalize functions, and add ordering when limit

--- src/services/sync/repositories/content-progress.ts
+++ src/services/sync/repositories/content-progress.ts
@@ -3,7 +3,7 @@ import ContentProgress from "../models/ContentProgress";
 
 // note naming, assumes pessimistic (confirms with server) by default,
 // optimistic opt-in (reads locally (except if never once synced))
-interface ContentProgressDTO {
+interface ProgressWriteDTO {
   contentId: number,
   state: string,
   progressPercent: number,
@@ -11,112 +11,84 @@ interface ContentProgressDTO {
   parentId?: number
 }
 
-const PARENT_ENUM_DEFAULT = null
-const PARENT_ENUM_LEARNING_PATH = 1
+enum PARENT_TYPE {
+  DEFAULT = 0,
+  LEARNING_PATH = 1,
+}
 
 export default class ProgressRepository extends SyncRepository<ContentProgress> {
   static create() {
     return new ProgressRepository(SyncRepository.getStore(ContentProgress))
   }
 
-  // todo is it useful to have limit here without order by ?
-  // todo create enum of parent type so param can input "method", and then can generalize functions
-
   async queryWhere(clauses: Record<string, any>[], limit: number = null) {
     return await this.readAllWhere(clauses, limit)
   }
 
-  async getAllMethodProgress(brand: string, limit: number = null) {
-    return await this.readAllWhere([{brand: brand}, {parent_type: PARENT_ENUM_LEARNING_PATH}], limit)
+  // all records: all parents null
+  // method records: parentType = 1, parentId = null
+  // LP records: parentType = 1, parentId = LP_id
+  async getAllProgress(
+      {
+        brand = null,
+        parentType = null,
+        parentId = null
+      }: {brand: string, parentType: number, parentId: number},
+      limit: number = null) {
+    let clauses = [];
+    if (brand) clauses.push({brand: brand});
+    if (parentType) clauses.push({parent_type: parentType});
+    if (parentId) clauses.push({parent_id: parentId});
+
+    return await this.readAllWhere(clauses, limit)
   }
 
-  async getAllNonMethodProgress(brand: string, limit: number = null) {
-    return await this.readAllWhere([{brand: brand}, {parent_type: null}], limit)
+  // does not allow retrieval of content_id and all its parent types
+  async getProgressByContentId(
+      {
+        contentId,
+        parentType = null,
+        parentId = null
+      }: {contentId: number, parentType: number, parentId: number}
+  ) {
+    const parentTypeVal = parentType ? parentType : PARENT_TYPE.DEFAULT;
+    const parentIdVal = parentId ? parentId : null;
+
+    return await this.readOne(ProgressRepository.generateId(contentId, parentTypeVal, parentIdVal))
   }
 
-  async getAllProgress(brand: string, limit: number = null) {
-    return await this.readAllWhere([{brand: brand}], limit)
+  async getProgressByContentIds(
+      {
+        contentIds,
+        parentType = null,
+        parentId = null
+      }: {contentIds: number[], parentType: number, parentId: number}) {
+    let clauses = [];
+    clauses.push({content_id: contentIds});
+    if (parentType) clauses.push({parent_type: parentType});
+    if (parentId) clauses.push({parent_id: parentId});
+
+    return await this.readAllWhere(clauses)
   }
 
-  async getLearningPathProgress(learningPathId: number) {
-    return await this.readAllWhere([{parent_type: PARENT_ENUM_LEARNING_PATH}, {parent_id: learningPathId}])
-  }
-
-  async getStandardProgressByContentId(contentId: number) {
-    return await this.readOne(ProgressRepository.generateId(contentId, PARENT_ENUM_DEFAULT, null))
-  }
-
-  async getStandardProgressByContentIds(contentIds: number[]) {
-    return await this.readSome(contentIds.map((c) => ProgressRepository.generateId(c, PARENT_ENUM_DEFAULT, null)))
-  }
-
-  async getMethodProgressByContentId(contentId: number, parentId: number) {
-    return await this.readOne(ProgressRepository.generateId(contentId, PARENT_ENUM_LEARNING_PATH, parentId))
-  }
-
-  async getMethodProgressByContentIds(contentIds: number[], parentId: number) {
-    return await this.readSome(contentIds.map((c) => ProgressRepository.generateId(c, PARENT_ENUM_LEARNING_PATH, parentId)))
-  }
-
-
-
-  async writeStandardProgress({
+  async writeProgress({
                         contentId,
                         state,
                         progressPercent,
-                      }: ContentProgressDTO) {
-    const progress = await this.store.upsertOne(ProgressRepository.generateId(contentId, PARENT_ENUM_DEFAULT, null), r => {
+                        parentType = PARENT_TYPE.DEFAULT,
+                        parentId = null,
+                      }: ProgressWriteDTO) {
+    const progress = await this.store.upsertOne(ProgressRepository.generateId(contentId, parentType, parentId), r => {
       r.content_id = contentId;
       r.state = state;
       r.progress_percent = progressPercent;
-      r.parent_type = PARENT_ENUM_DEFAULT;
-      r.parent_id = null;
-    })
-    return await this.pushOneEagerlyById(progress.id)
-  }
-
-  async writeMethodProgress({
-                              contentId,
-                              state,
-                              progressPercent,
-                              parentId,
-  }: ContentProgressDTO) {
-    const progress = await this.store.upsertOne(ProgressRepository.generateId(contentId, PARENT_ENUM_LEARNING_PATH, parentId), r => {
-      r.content_id = contentId;
-      r.state = state;
-      r.progress_percent = progressPercent;
-      r.parent_type = PARENT_ENUM_LEARNING_PATH;
+      r.parent_type = parentType;
       r.parent_id = parentId;
     })
     return await this.pushOneEagerlyById(progress.id)
   }
 
-  async getProgressOptimistic(contentId: number) {
-    return await this.readOne(ProgressRepository.generateId(contentId))
-  }
-
-  async getProgressesOptimistic(contentIds: number[]) {
-    return await this.readSome(contentIds.map(ProgressRepository.generateId))
-  }
-
-  async areLikedOptimistic(contentIds: number[]) {
-    return await this.existSome(contentIds.map(ProgressRepository.generateId))
-  }
-
-  async likeOptimisticEager(contentId: number) {
-    const like = await this.store.upsertOne(ProgressRepository.generateId(contentId), r => {
-      r.content_id = contentId;
-    })
-
-    return await this.pushOneEagerlyById(like.id)
-  }
-
-  async unlikeOptimisticEager(contentId: number) {
-    const id = await this.store.deleteOne(ProgressRepository.generateId(contentId))
-    return await this.pushOneEagerlyById(id)
-  }
-
-  private static generateId(contentId: number, parentType: number = null, parentId: number = null) {
+  private static generateId(contentId: number, parentType: number, parentId: number) {
     return contentId.toString() + ":" + parentType.toString() + ":" + parentId.toString();
   }
 }
--- src/services/sync/store/index.ts
+++ src/services/sync/store/index.ts
@@ -397,11 +397,10 @@ export default class SyncStore<TModel extends BaseModel = BaseModel> {
 
   async readAllWhere(clauses: Record<string, any>[], limit: number = null) {
     const args: any[] = clauses.map(q => {
-      return Q.where(q.key, q.value)
+      return (q.value instanceof Array) ? Q.where(q.key, Q.oneOf(q.value)) : Q.where(q.key, q.value)
     })
-    if (limit) {
-      args.push(Q.take(limit))
-    }
+    if (limit) args.push(Q.take(limit), Q.sortBy('updated_at', 'desc'))
+
     const records = await this.queryRecords(...args);
     return records.map(record => this.modelSerializer.toPlainObject(record))
   }
