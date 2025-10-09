b1fd2dff8ed22ddca003f850b6cde4186de5592a
Sheldon Baker <sbaker@musora.com>
2025-10-09 11:08:07 -0700

    account for db errors before manager instantiation

--- src/services/sync/telemetry/sampling.ts
+++ src/services/sync/telemetry/sampling.ts
@@ -1,13 +1,19 @@
-import { SYNC_TELEMETRY_TRACE_PREFIX, type SentryBrowserOptions } from './index'
-import SyncManager from '../manager'
+import { SyncTelemetry, SYNC_TELEMETRY_TRACE_PREFIX, type SentryBrowserOptions } from '.'
 import { SyncError } from '../errors'
 
-export const syncSentryBeforeSend: SentryBrowserOptions['beforeSend'] = (event, hint) => {
-  if (event.logger === 'console' && SyncManager.getInstance().telemetry.shouldIgnoreConsole()) {
+type ReturnsUndefined<T extends (...args: any[]) => any> = (...args: Parameters<T>) => ReturnType<T> | undefined
+
+export const syncSentryBeforeSend: ReturnsUndefined<NonNullable<SentryBrowserOptions['beforeSend']>> = (event, hint) => {
+  if (event.logger === 'console' && SyncTelemetry.getInstance().shouldIgnoreConsole()) {
     return null
   }
 
   if (hint?.originalException instanceof SyncError) {
+    if (hint.originalException.isReported()) {
+      return null
+    }
+
+    // populate event with error data Sentry would otherwise ignore
     event.extra = {
       ...event.extra,
       details: hint.originalException.getDetails()
@@ -19,24 +25,43 @@ export const syncSentryBeforeSend: SentryBrowserOptions['beforeSend'] = (event, 
   return undefined
 }
 
-export const syncSentryTracesSampler: SentryBrowserOptions['tracesSampler'] = (context) => {
-  if (!context.name.startsWith(SYNC_TELEMETRY_TRACE_PREFIX)) {
-    return undefined
+export const syncSentryBeforeSendTransaction: ReturnsUndefined<NonNullable<SentryBrowserOptions['beforeSendTransaction']>> = (event, hint) => {
+  if (event.contexts?.trace?.op?.startsWith(SYNC_TELEMETRY_TRACE_PREFIX)) {
+    // filter out noisy empty sync traces
+    if (event.contexts.trace.op === `${SYNC_TELEMETRY_TRACE_PREFIX}sync` && event.spans?.length === 0) {
+      return null
+    }
   }
 
-  const { parentSampled, attributes } = context
+  return undefined
+}
 
-  if (parentSampled) {
-    return true
+// sentry doesn't bother to expose your chosen environment in tracesSampler
+// so we have to make consumers pass in our greedy option
+export const createSyncSentryTracesSampler = (greedy = false) => {
+  const sampler: ReturnsUndefined<NonNullable<SentryBrowserOptions['tracesSampler']>> = (context) => {
+    if (!context.name.startsWith(SYNC_TELEMETRY_TRACE_PREFIX)) {
+      return undefined
+    }
+
+    if (greedy) {
+      return true
+    }
+
+    const { parentSampled, attributes } = context
+
+    if (parentSampled) {
+      return true
+    }
+
+    if (attributes?.userId) {
+      return userBucketedSampler(attributes.userId as string | number, 0.1)
+    }
+
+    return false
   }
 
-  // todo - return 1.0 in development+staging
-
-  if (attributes?.userId) {
-    return userBucketedSampler(attributes.userId as string | number, 0.1)
-  }
-
-  return false
+  return sampler
 }
 
 /**
