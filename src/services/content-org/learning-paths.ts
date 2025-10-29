6a49324edb2c1c205d852e437a0ee61297cecf1f
Daniel Austin <51715647+Meowbox64@users.noreply.github.com>
2025-10-28 17:59:59 -0700

    feat: getNextLearningPathLessonsForMethod function

--- src/services/content-org/learning-paths.ts
+++ src/services/content-org/learning-paths.ts
@@ -1,105 +1,47 @@
-import ProgressRepository from "../sync/repositories/content-progress";
 import {findIncompleteLesson} from "../userActivity";
-
-const PARENT_TYPE_LEARNING_PATH = 'learning-path';
-
-const DATA_KEY_STATE = 'state';
 
 interface dailySessionItem {
   learningPathId: number,
-  contentIds: number
+  contentIds: number[]
 }
 
-
-export async function getNextLearningPathLesson( //there's gotta be a better name for this
-  progressData: object[],
-  brand: string,
-  activePathId: number,
-  dailySession: dailySessionItem[],
-  methodStructure: object
-): Promise<object>
-{
-  let contentData: any
-  // if no progress on method at all
+export async function getNextLearningPathLessonsForMethod(
+    progressData: object[]|null,
+    activePath: dailySessionItem|null, //turns out this is the right structure
+    dailySession: dailySessionItem[]|null,
+): Promise<dailySessionItem[] | dailySessionItem | [] | null> {
   if (!progressData || Object.keys(progressData).length === 0) {
-    // sanity method IV
-    // if we're just returning ids from this then how do we indicate that this
-    // return needs to be a sanity fetch, without the actual contentId?
-    return {}
+    return null
 
   } else {
-    // const activePathId = await getActiveLearningPath(brand)
-    // const dailySession = getDailySession(brand)
+    const dailySessionIds = dailySession?.map((item: dailySessionItem) => item.contentIds).flat()
+    const dailySessionProgress = dailySessionIds ? progressData.filter((item: any) => dailySessionIds.includes(item.content_id)) : null
 
-    // get progress for daily session ids
-    const dailySessionIds = dailySession.map((item: dailySessionItem) => item.contentIds)
-    const dailySessionProgress = await getKeyedProgress(dailySessionIds, PARENT_TYPE_LEARNING_PATH)
+    const isDailyComplete = dailySessionProgress ? areAllCompleted(dailySessionProgress) : false
 
-    const isDailyComplete = dailySessionProgress ? checkIfDailyComplete(dailySessionProgress) : false
-    if (isDailyComplete) {
-      const activePathLessons = await getLearningPathLessons(activePathId)
+    if (!isDailyComplete) {
+      return dailySession
 
-      const mergedProgressData = activePathLessons.map(id => {
-        return progressData[id] ? progressData[id][DATA_KEY_STATE] : ''
-      })
-
-      const nextLesson = findIncompleteLesson(mergedProgressData, null, 'learning-path')
-
-      contentData = [{ collectionId: activePathId, ids: nextLesson }]
     } else {
-      contentData = dailySessionProgress
+      // active path is set if daily session is.
+      const learningPathProgress = progressData.filter((item: any) => activePath.contentIds.includes(item.content_id))
+
+      if (areAllCompleted(learningPathProgress)) {
+        return []
+      }
+
+      const nextLesson = findIncompleteLesson(learningPathProgress, null, 'learning-path')
+      return {learningPathId: activePath.learningPathId, contentIds: [nextLesson]} as dailySessionItem
     }
-    return {data: contentData, isDailyComplete: isDailyComplete}
   }
 }
 
-function checkIfDailyComplete(dailySession: any[]): boolean
+function areAllCompleted(progress: any[]): boolean
 {
-  dailySession.forEach(session => {
-    if (session.status !== 'completed') {
+  progress.forEach(item => {
+    if (item.status !== 'completed') {
       return false
     }
   });
   return true
-}
-
-// export async function getActiveLearningPathLessons(brand: string): Promise<{parent: number, lessons: number[]}>
-// {
-//   const activeLearningPath = await getActiveLearningPath(brand)
-//
-//   getLearningPathLessons(activeLearningPath)
-//
-//   return {parent: activeLearningPath, lessons: [1,2,3]}
-// }
-
-async function getActiveLearningPath(brand: string): Promise<number>
-{
-  // fetch from the cache
-
-  // if no cache value then fetch from BE
-  return 0
-}
-
-async function getLearningPathLessons(learningPathId: number): Promise<number[]>
-{
-  // fetch from the cache
-}
-
-function getDailySession(brand: string): object[]|[]
-{
-  // get from cache /// if none get from BE
-
-  // can be < 3 if at end of last LP
-  return [{id: 1, collectionId: 1}, {id: 2, collectionId: 1}, {id: 3, collectionId: 2}]
-}
-
-// this feels weird. should this be in the repository?
-async function getKeyedProgress(ids: number[], parentType: string): Promise<object[]>
-{
-  const records = await ProgressRepository.create().getProgressByContentIds({
-    contentIds: ids,
-    parentType: parentType,
-    parentId: null,
-  })
-  return ids.map(id => records.find(r => r.content_id === id))
 }
