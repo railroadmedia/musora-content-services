19a22ee560af64448b7a40a6784d4bba9d0794b4
David Douglas <ddouglas@musora.com>
2025-11-05 09:54:29 -0800

    fix(T3PS-864): Remove sneaky console.log

--- src/services/user/payments.ts
+++ src/services/user/payments.ts
@@ -67,7 +67,6 @@ export async function fetchCustomerPayments(
   if (options.cursor) {
     params.append('cursor', options.cursor)
   }
-  console.log(options)
   const queryString = params.toString()
   const url = queryString ? `/api/customer/orders/v1?${queryString}` : '/api/customer/orders/v1'
 
