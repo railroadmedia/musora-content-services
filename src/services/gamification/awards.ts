ed49d32078d9408ef6502a0d113bad01ffa23471
Alexandre Severo <alexandre.severo@me.com>
2025-11-13 08:00:16 -0800

    fix(awards): optional userId parameter

--- src/services/gamification/awards.ts
+++ src/services/gamification/awards.ts
@@ -11,7 +11,7 @@ const baseUrl = `/api/gamification`
 export interface Award {
   id: number
   user_id: number
-  completed_at: string          // ISO-8601 timestamp
+  completed_at: string // ISO-8601 timestamp
   completion_data: Object
   award_id: number
   type: string
@@ -23,7 +23,7 @@ export interface Certificate {
   id: number
   user_name: string
   user_id: number
-  completed_at: string          // ISO-8601 timestamp
+  completed_at: string // ISO-8601 timestamp
   message: string
   award_id: number
   type: string
@@ -49,18 +49,22 @@ export interface Certificate {
  * (Alexandre: I'm doing it in a different branch/PR: https://github.com/railroadmedia/musora-content-services/pull/349)
  * NOTE: This function still expects brand because FE passes the argument. It is ignored for now
  *
- * @param {number} userId - The user ID. If not provided, the authenticated user is used instead.
- * @param {string} _brand - The brand to fetch the awards for.
+ * @param {number} [userId] - The user ID. If not provided, the authenticated user is used instead.
+ * @param {string} [_brand] - The brand to fetch the awards for.
  * @param {number|null} [page=1] - Page attribute for pagination
  * @param {number|null} [limit=5] - Limit how many items to return
  * @returns {Promise<PaginatedResponse<Award>>} - The awards for the user.
  */
 export async function fetchAwardsForUser(
-  userId: number,
-  _brand: string,
+  userId?: number,
+  _brand?: string,
   page: number = 1,
   limit: number = 5
 ): Promise<PaginatedResponse<Award>> {
+  if (!userId) {
+    userId = globalConfig.sessionConfig.userId
+  }
+
   const httpClient = new HttpClient(globalConfig.baseUrl, globalConfig.sessionConfig.token)
   const response = await httpClient.get<PaginatedResponse<Award>>(
     `${baseUrl}/v1/users/${userId}/awards?limit=${limit}&page=${page}`
@@ -79,9 +83,7 @@ export async function fetchAwardsForUser(
  * @param {number} guidedCourseLessonId - The guided course lesson Id
  * @returns {Promise<Award>} - The award data for a given award and given user.
  */
-export async function getAwardDataForGuidedContent(
-  guidedCourseLessonId,
-): Promise<Award> {
+export async function getAwardDataForGuidedContent(guidedCourseLessonId): Promise<Award> {
   const httpClient = new HttpClient(globalConfig.baseUrl, globalConfig.sessionConfig.token)
   const response = await httpClient.get<Award>(
     `${baseUrl}/v1/users/guided_course_award/${guidedCourseLessonId}`
