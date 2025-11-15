9ef53720e5afc77225dc0894757bf3eb345387a8
Sheldon Baker <sbaker@musora.com>
2025-11-14 16:09:28 -0800

    includes created_at and updated_at in serialized model

--- src/services/sync/serializers/model.ts
+++ src/services/sync/serializers/model.ts
@@ -1,4 +1,5 @@
 import { Model, RecordId } from "@nozbe/watermelondb"
+import BaseModel from "../models/Base"
 
 export type ModelSerialized<TModel extends Model> = ExtractGetters<TModel>
 type ExtractGetters<T> = {
@@ -12,11 +13,14 @@ type ExtractGetters<T> = {
 export default class ModelSerializer<TModel extends Model = Model> {
   toPlainObject(record: TModel) {
     const proto = Object.getPrototypeOf(record)
-    const keys = Object.getOwnPropertyNames(proto)
+    const keys = [
+      ...Object.getOwnPropertyNames(proto),
+      ...Object.getOwnPropertyNames(BaseModel.prototype)
+    ]
 
     const result = {}
     for (const key of keys) {
-      const desc = Object.getOwnPropertyDescriptor(proto, key)
+      const desc = Object.getOwnPropertyDescriptor(proto, key) || Object.getOwnPropertyDescriptor(BaseModel.prototype, key)
       if (desc?.get) {
         result[key] = desc.get.call(record)
       }
