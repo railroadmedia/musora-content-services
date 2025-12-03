7b2a3e8ff0db7b308df59ba1e8dbd8f149984256
rob <robert@musora.com>
2025-12-03 13:08:49 -0800

    fix: add intiailize test logic for watermelon

--- test/initializeTests.js
+++ test/initializeTests.js
@@ -1,5 +1,29 @@
 import { globalConfig, initializeService } from '../src'
 import { LocalStorageMock } from './localStorageMock'
+import { SyncManager, SyncContext } from '../src/services/sync/index'
+import {
+  BaseSessionProvider,
+  BaseConnectivityProvider,
+  BaseDurabilityProvider,
+  BaseTabsProvider,
+  BaseVisibilityProvider,
+} from '../src/services/sync/context/providers/'
+import adapterFactory from '../src/services/sync/adapters/factory'
+
+import LokiJSAdapter from '../src/services/sync/adapters/lokijs'
+import EventEmitter from '../src/services/sync/utils/event-emitter'
+import { InitialStrategy, PollingStrategy } from '../src/services/sync/strategies/index'
+import { SyncTelemetry, SentryLike } from '../src/services/sync/telemetry/index'
+import {
+  ContentLike,
+  ContentProgress,
+  Practice,
+  PracticeDayNote,
+} from '../src/services/sync/models/index'
+import syncDatabaseFactory from '../src/services/sync/database/factory'
+
+import syncAdapter, { SyncAdapterEventBus } from './adapter'
+
 const railContentModule = require('../src/services/railcontent.js')
 let token = null
 let userId = process.env.RAILCONTENT_USER_ID ?? null
@@ -43,7 +67,7 @@ export async function initializeTestService(useLive = false, isAdmin = false) {
       baseUrl: process.env.RAILCONTENT_BASE_URL || 'https://test.musora.com',
       token: token,
       userId: userId,
-      authToken: token
+      authToken: token,
     },
     sessionConfig: { token: token, userId: userId, authToken: token },
     baseUrl: process.env.RAILCONTENT_BASE_URL,
@@ -51,9 +75,89 @@ export async function initializeTestService(useLive = false, isAdmin = false) {
     isMA: true,
   }
   initializeService(config)
-
   // Mock user permissions
   let permissionsMock = jest.spyOn(railContentModule, 'fetchUserPermissionsData')
   let permissionsData = { permissions: [108, 91, 92], isAdmin: isAdmin }
   permissionsMock.mockImplementation(() => permissionsData)
+
+  const dummySentry = {
+    captureException: () => '',
+    addBreadcrumb: () => {},
+    startSpan: (options, callback) => {
+      // Return a mock span object with the properties Sentry expects
+      const mockSpan = {
+        _spanId: 'mock-span-id',
+        end: () => {},
+        setStatus: () => {},
+        setData: () => {},
+        setAttribute: () => {},
+      }
+
+      if (callback) {
+        return callback(mockSpan)
+      }
+      return mockSpan
+    },
+  }
+
+  if (SyncManager.instance) {
+    return
+  }
+
+  SyncTelemetry.setInstance(new SyncTelemetry(userId, { Sentry: dummySentry }))
+
+  const adapterBus = new SyncAdapterEventBus()
+  const adapter = syncAdapter(userId, adapterBus)
+  const db = syncDatabaseFactory(adapter)
+
+  const context = new SyncContext({
+    session: {
+      getClientId: () => 'test-client-id',
+      getSessionId: () => null,
+      start: () => {},
+      stop: () => {},
+    },
+    connectivity: {
+      getValue: () => true,
+      subscribe: () => () => {},
+      notifyListeners: () => {},
+      start: () => {},
+      stop: () => {},
+    },
+    visibility: {
+      getValue: () => true,
+      subscribe: () => () => {},
+      notifyListeners: () => {},
+      start: () => {},
+      stop: () => {},
+    },
+    tabs: {
+      hasOtherTabs: () => false,
+      broadcast: () => {},
+      subscribe: () => () => {},
+      start: () => {},
+      stop: () => {},
+    },
+    durability: {
+      getValue: () => true,
+      start: () => {},
+      stop: () => {},
+    },
+  })
+  const manager = new SyncManager(context, db)
+
+  const initialStrategy = manager.createStrategy(InitialStrategy)
+  const aggressivePollingStrategy = manager.createStrategy(PollingStrategy, 3600_000)
+
+  manager.syncStoresWithStrategies(
+    manager.storesForModels([ContentLike, ContentProgress, Practice, PracticeDayNote]),
+    [initialStrategy, aggressivePollingStrategy]
+  )
+
+  manager.protectStores(
+    manager.storesForModels([ContentLike, ContentProgress, Practice, PracticeDayNote]),
+    []
+  )
+
+  SyncManager.assignAndSetupInstance(manager)
 }
