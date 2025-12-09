1ea9ea69a9282cc6cea8bdc267014e18d896136b
Alexandre Severo <alexandre.severo@me.com>
2025-12-09 15:58:58 -0800

    chore(groq): new filter builder

--- src/services/content/instructor.ts
+++ src/services/content/instructor.ts
@@ -2,11 +2,12 @@
  * @module Instructor
  */
 import { FilterBuilder } from '../../filterBuilder.js'
-import { filtersToGroq, getFieldsForContentType } from '../../contentTypeConfig.js'
+import { getFieldsForContentType } from '../../contentTypeConfig.js'
 import { fetchSanity, getSortOrder } from '../sanity.js'
 import { Lesson } from './content'
 import { BuildQueryOptions, query } from '../../lib/sanity/query'
 import { Brands } from '../../lib/brands'
+import { Filters as f } from '../../lib/sanity/filter'
 
 export interface Instructor {
   lessonCount: number
@@ -36,27 +37,28 @@ export async function fetchInstructors(
   brand: Brands | string,
   options: BuildQueryOptions
 ): Promise<Instructors> {
-  const lessonFilter = await new FilterBuilder(`brand == "${brand}" && references(^._id)`, {
-    bypassPermissions: true,
-  }).buildFilter()
+  const type = f.type('instructor')
+  const lesson = f.combine(f.brand(brand), f.referencesParent())
+  const lessonCount = `count(*[${lesson}])`
+  const postFilter = `lessonCount > 0`
 
   const data = query()
-    .and(`_type == "instructor"`)
+    .and(type)
     .order(options?.sort || 'lower(name) asc')
     .slice(options?.offset || 0, (options?.offset || 0) + (options?.limit || 20))
     .select(
       'name',
       `"slug": slug.current`,
       `"thumbnail": thumbnail_url.asset->url`,
-      `"lessonCount": count(*[${lessonFilter}])`
+      `"lessonCount": ${lessonCount}`
     )
-    .postFilter(`lessonCount > 0`)
+    .postFilter(postFilter)
     .build()
 
   const total = query()
-    .and(`_type == "instructor"`)
-    .select(`"lessonCount": count(*[${lessonFilter}])`)
-    .postFilter(`lessonCount > 0`)
+    .and(type)
+    .select(`"lessonCount": ${lessonCount}`)
+    .postFilter(postFilter)
     .build()
 
   const q = `{
@@ -83,14 +85,11 @@ export async function fetchInstructorBySlug(
   slug: string,
   brand?: Brands | string
 ): Promise<Instructor | null> {
-  const brandFilter = brand ? `brand == "${brand}" && ` : ''
-  const filter = await new FilterBuilder(`${brandFilter} references(^._id)`, {
-    bypassPermissions: true,
-  }).buildFilter()
+  const filter = f.combine(brand ? f.brand(brand) : f.empty, f.referencesParent())
 
   const q = query()
-    .and(`_type == "instructor"`)
-    .and(`slug.current == "${slug}"`)
+    .and(f.type('instructor'))
+    .and(f.slug(slug))
     .select(
       'name',
       `"slug": slug.current`,
@@ -143,22 +142,24 @@ export async function fetchInstructorLessons(
     includedFields = [],
   }: InstructorLessonsOptions = {}
 ): Promise<InstructorLessons> {
-  const fieldsString = getFieldsForContentType() as string
-  const searchFilter = searchTerm ? `&& title match "${searchTerm}*"` : ''
-  const includedFieldsFilter = includedFields.length > 0 ? filtersToGroq(includedFields) : ''
-  const filter = `brand == '${brand}' ${searchFilter} ${includedFieldsFilter} && references(*[_type=='instructor' && slug.current == '${slug}']._id)`
-  const filterWithRestrictions = await new FilterBuilder(filter).buildFilter()
-
   sort = getSortOrder(sort, brand)
 
+  const fieldsString = getFieldsForContentType() as string
+
+  const restrictions = await f.combineAsync(f.status(), f.permissions(), f.publishedDate())
+
   const data = query()
-    .and(filterWithRestrictions)
+    .and(f.brand(brand))
+    .and(f.searchMatch('title', searchTerm))
+    .and(f.includedFields(includedFields))
+    .and(f.referencesIDWithFilter(f.combine(f.type('instructor'), f.slug(slug))))
+    .and(restrictions)
     .order(sort)
     .slice(offset, offset + limit)
     .select(...(fieldsString ? [fieldsString] : []))
     .build()
 
-  const total = query().and(filterWithRestrictions).build()
+  const total = query().and(restrictions).build()
 
   const q = `{
     "data": ${data},
