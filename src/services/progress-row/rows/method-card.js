25efc43b6c07172db2f20d51d5b79ac9fc204eef
Daniel A <51715647+Meowbox64@users.noreply.github.com>
2026-01-19 09:58:39 -0800

    feat(TP-1060): method data caching in mcs (#701)
    * feat: adds cache header to HttpClient
    refactors getDailySession to send full datetime to BE
    
    * fix: format userDate to just date + timezone offset for BE
    
    * fix: dont await triggered GETs
    
    * feat: abstracts GET request into a helper function that returns promise, and uses that for GET requests within POST
    
    * refactor: change options
    
    * feat: storing GET promises for multiple requests

--- src/services/progress-row/rows/method-card.js
+++ src/services/progress-row/rows/method-card.js
@@ -3,7 +3,6 @@
  */
 
 import { getActivePath, fetchLearningPathLessons } from '../../content-org/learning-paths'
-import { getToday } from '../../dateUtils.js'
 import { fetchMethodV2IntroVideo } from '../../sanity'
 import { getProgressState } from '../../contentProgress'
 import {COLLECTION_TYPE, STATE} from '../../sync/models/ContentProgress'
@@ -45,7 +44,7 @@ export async function getMethodCard(brand) {
     const learningPath = await fetchLearningPathLessons(
       activeLearningPath.active_learning_path_id,
       brand,
-      getToday()
+      new Date()
     )
 
     if (!learningPath) {
