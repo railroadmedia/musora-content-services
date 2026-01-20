fac388d506550838c2ffc3708dbe8b194d9695cb
Sheldon Baker <sbaker@musora.com>
2026-01-20 10:42:44 -0800

    rely on namespaced dbs

--- src/services/sync/database/factory.ts
+++ src/services/sync/database/factory.ts
@@ -1,12 +1,13 @@
 import type { DatabaseAdapter } from '../adapters/factory'
 import { Database } from '@nozbe/watermelondb'
 import * as modelClasses from '../models'
+import type { SyncUserScope } from '../index'
 
-export default function syncDatabaseFactory(adapter: () => DatabaseAdapter, { onInitError }: { onInitError?: (error: Error) => void } = {}) {
-  return () => {
+export default function syncDatabaseFactory(adapter: (userScope: SyncUserScope) => DatabaseAdapter, { onInitError }: { onInitError?: (error: Error) => void } = {}) {
+  return (userScope: SyncUserScope) => {
     try {
       return new Database({
-        adapter: adapter(),
+        adapter: adapter(userScope),
         modelClasses: Object.values(modelClasses)
       })
     } catch (error) {
