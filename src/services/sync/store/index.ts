cc5a54f247491e7733b0b3c492ab397847759383
Sheldon Baker <sheldon@sheld.sh>
2026-02-10 14:46:16 -0800

    fix: returns promise on public pull method (#802)

--- src/services/sync/store/index.ts
+++ src/services/sync/store/index.ts
@@ -132,13 +132,11 @@ export default class SyncStore<TModel extends BaseModel = BaseModel> {
     }, { table: this.model.table, reason })
   }
 
-  async requestPull(reason: string) {
-    inBoundary(ctx => {
-      this.telemetry.trace(
+  async pull(reason: string) {
+    return inBoundary(ctx => {
+      return this.telemetry.trace(
         { name: `sync:${this.model.table}`, op: 'pull', attributes: { ...ctx, ...this.context.session.toJSON() } },
-        async span => {
-          await this.pullRecordsWithRetry(span)
-        }
+        async span => await this.pullRecords(span)
       )
     }, { table: this.model.table, reason })
   }
@@ -147,9 +145,7 @@ export default class SyncStore<TModel extends BaseModel = BaseModel> {
     inBoundary(ctx => {
       this.telemetry.trace(
         { name: `sync:${this.model.table}`, op: 'push', attributes: { ...ctx, ...this.context.session.toJSON() } },
-        async span => {
-          await this.pushUnsyncedWithRetry(span, { type: 'push-request', reason })
-        }
+        async span => await this.pushUnsyncedWithRetry(span, { type: 'push-request', reason })
       )
     }, { table: this.model.table, reason })
   }
