25882a9c7ab162cdbc6d9fec96b53459b328feb2
Daniel A <51715647+Meowbox64@users.noreply.github.com>
2026-03-31 10:11:09 -0700

    fix(BR-531): fix award Days Practiced calculation (#887)
    * fix: remove ms compensation
    
    * fix: minutes plurality

--- src/services/awards/internal/message-generator.js
+++ src/services/awards/internal/message-generator.js
@@ -2,16 +2,18 @@ export class AwardMessageGenerator {
   /** @returns {string} */
   static generatePopupMessage(completionData) {
     const { days_user_practiced, practice_minutes, content_title } = completionData
+    const minutesLabel = practice_minutes === 1 ? 'minute' : 'minutes'
     const daysLabel = days_user_practiced === 1 ? 'day' : 'days'
 
-    return `You received this award for completing ${content_title}! You practiced a total of ${practice_minutes} minutes over the past ${days_user_practiced} ${daysLabel}.`
+    return `You received this award for completing ${content_title}! You practiced a total of ${practice_minutes} ${minutesLabel} over the past ${days_user_practiced} ${daysLabel}.`
   }
 
   /** @returns {string} */
   static generateCertificateMessage(completionData, awardCustomText) {
     const { content_title, practice_minutes } = completionData
+    const minutesLabel = practice_minutes === 1 ? 'minute' : 'minutes'
     const customText = awardCustomText ? ` ${awardCustomText}` : ''
 
-    return `You practiced for a total of ${practice_minutes} minutes during ${content_title}, earning your official certificate of completion.${customText} Well Done!`
+    return `You practiced for a total of ${practice_minutes} ${minutesLabel} during ${content_title}, earning your official certificate of completion.${customText} Well Done!`
   }
 }
