c567d227a4f3bdb9f2f1c95364d423e67b1b214f
rob <robert@musora.com>
2026-05-05 15:08:09 -0700

    fix: update sanity url to v4

--- src/services/sanity.js
+++ src/services/sanity.js
@@ -1589,7 +1589,7 @@ export async function fetchSanity(
   }
   const perspective = globalConfig.sanityConfig.perspective ?? 'published'
   const api = globalConfig.sanityConfig.useCachedAPI ? 'apicdn' : 'api'
-  const baseUrl = `https://sanity.musora.com/${globalConfig.sanityConfig.projectId}/${api}/v${globalConfig.sanityConfig.version}/${globalConfig.sanityConfig.dataset}/v3?perspective=${perspective}`
+  const baseUrl = `https://sanity.musora.com/${globalConfig.sanityConfig.projectId}/${api}/v${globalConfig.sanityConfig.version}/${globalConfig.sanityConfig.dataset}/v4?perspective=${perspective}`
 
   try {
     const encodedQuery = encodeURIComponent(query)
